688d14db7c5e12f852faad2bb724026b796966da
ichsanputr <ngekode24@gmail.com>
2025-06-21 21:11:28 +0700

    chore: add deploy workflows gh pages

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,9 @@ export default defineConfig({
     port: 3000,
     host: true,
   },
+  base: process.env.NODE_ENV === 'production' ? '/sveltedashboard/' : '/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
