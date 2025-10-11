dfe20504f335605c1e7c2f15dfe5272365a2adb6
shishyan <nagaraj957@gmail.com>
2025-10-12 00:40:47 +0530

    Update deployment: Copy latest build files to gh-pages root
    - Build latest changes with navigation fixes
    - Copy dist/ contents to root directory for GitHub Pages deployment
    - Remove dist/ folder after copying
    - Ensure .nojekyll file is present for proper asset serving
    - Navigation components now use goto('') for correct base path routing

--- .svelte-kit/output/client/_app/immutable/entry/start.CmTIcRkA.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"../chunks/BaFmVGxF.js";import{x as t}from"../chunks/Bv3TmAC8.js";export{t as load_css,r as start};
--- .svelte-kit/output/server/nodes/130.js
+++ .svelte-kit/output/server/nodes/130.js
@@ -3,6 +3,6 @@
 export const index = 130;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/todo/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/130.C-Am27B5.js","_app/immutable/chunks/Bzak7iHL.js","_app/immutable/chunks/69_IOA4Y.js","_app/immutable/chunks/DIeogL5L.js","_app/immutable/chunks/39fK5dpH.js","_app/immutable/chunks/CirWT1vm.js","_app/immutable/chunks/CVl-uwuJ.js","_app/immutable/chunks/BaFmVGxF.js","_app/immutable/chunks/Bv3TmAC8.js","_app/immutable/chunks/DWPppJ2y.js","_app/immutable/chunks/C02VnCvr.js"];
+export const imports = ["_app/immutable/nodes/130.Ds8Fbl2n.js","_app/immutable/chunks/Bzak7iHL.js","_app/immutable/chunks/69_IOA4Y.js","_app/immutable/chunks/DIeogL5L.js","_app/immutable/chunks/39fK5dpH.js","_app/immutable/chunks/CirWT1vm.js","_app/immutable/chunks/CVl-uwuJ.js","_app/immutable/chunks/C11exIVZ.js","_app/immutable/chunks/zYgbgurY.js","_app/immutable/chunks/DWPppJ2y.js","_app/immutable/chunks/BVEgw-fk.js"];
 export const stylesheets = [];
 export const fonts = [];
