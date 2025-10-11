10605fcadc062151a7fae3b1f9f4ee1973b140ef
shishyan <nagaraj957@gmail.com>
2025-10-11 23:36:29 +0530

    Clean redeploy: fresh build with updated service worker cache version v1.0.2

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,7 +1,7 @@
 // Service Worker for Divine Nest PWA
-const CACHE_NAME = 'divine-nest-v1.0.1';
-const STATIC_CACHE = 'divine-nest-static-v1.0.1';
-const DYNAMIC_CACHE = 'divine-nest-dynamic-v1.0.1';
+const CACHE_NAME = 'divine-nest-v1.0.2';
+const STATIC_CACHE = 'divine-nest-static-v1.0.2';
+const DYNAMIC_CACHE = 'divine-nest-dynamic-v1.0.2';
 
 // Get the base path from the service worker's location
 const BASE_PATH = self.location.pathname.replace('/service-worker.js', '');
