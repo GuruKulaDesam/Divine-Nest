e66586aca328381405a36c6abad37a46ab1e5d1a
shishyan <nagaraj957@gmail.com>
2025-10-11 20:55:05 +0530

    Add built files and 404.html for GitHub Pages SPA routing

---
+++ dist/_app/immutable/entry/start.C3W8QeJC.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/DI5Xoqzd.js";import{x as t}from"../chunks/DP36L0pH.js";export{t as load_css,r as start};
---
+++ dist/_app/immutable/nodes/93.Bizowk4J.js
@@ -0,0 +1,1 @@
+import"../chunks/Bzak7iHL.js";import"../chunks/69_IOA4Y.js";import{V as p}from"../chunks/D8EljAc8.js";function m(o){p(o)}export{m as component};
